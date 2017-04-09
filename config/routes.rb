bca35c5334e24415eaacfed28d4e7efaf9c8fae3
carotene4035 <karoten512@gmail.com>
2017-04-09 14:57:09 +0000

    サインイン後にtopページへ推移するようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'top/index'
+
   devise_for :users
+
+  root 'top#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
