90da29fb246e959450a6f418751429cbc1aa28c0
carotene4035 <karoten512@gmail.com>
2017-04-10 12:20:49 +0000

    ログイン後、画像一覧を表示する用に修正。

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20170410113236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "pictures", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image_url"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -34,12 +41,5 @@ ActiveRecord::Schema.define(version: 20170410113236) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "pictures", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "image_url"
-  end
 
 end
